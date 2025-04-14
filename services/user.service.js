669459e29914be20396d8e75ec98560587cd1713
Boaz Teller <boaz.teller@gmail.com>
2025-04-14 15:22:26 +0300

    feat: add signup and remove to user service and refactor existing methods

--- services/user.service.js
+++ services/user.service.js
@@ -1,28 +1,81 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 import { utilService } from './util.service.js'
 
 const USERS_FILE = './data/users.json'
+const gUsers = utilService.readJsonFile(USERS_FILE)
 
 export const userService = {
     query,
+    signup,
     getById,
-    getByUsername
+    getByUsername,
+    remove
 }
 
 function query() {
-    return utilService.readJsonFile(USERS_FILE)
-        .then(users => users)
-        .catch(err => {
-            throw new Error('Cannot load users:', err)
+    const usersToReturn = gUsers.map(user =>
+        ({ _id: user._id, fullname: user.fullname })
+    )
+    
+    return Promise.resolve(usersToReturn)
+}
+
+function signup(user) {
+    return getByUsername(user.username)
+        .then(existingUser => {
+            if (existingUser) return Promise.reject('Username already taken')
+
+            user._id = utilService.makeId()
+            gUsers.push(user)
+
+            return _saveUsersToFile()
+                .then(() => {
+                    const { password, ...userToReturn } = user
+                    return userToReturn
+                })
         })
 }
 
 function getById(userId) {
-    return query()
-        .then(users => users.find(user => user._id === userId))
+    const user = gUsers.find(user => user._id === userId)
+    if (!user) return Promise.reject('User not found')
+
+    // Use destructuring to exclude password from the returned object
+    const { password, ...userToReturn } = user
+
+    return Promise.resolve(userToReturn)
 }
 
 function getByUsername(username) {
-    return query()
-        .then(users => users.find(user => user.username === username))
+    const user = gUsers.find(user => user.username === username)
+    
+    return Promise.resolve(user)
 }
+
+function remove(userId) {
+    const userIdx = gUsers.findIndex(user => user._id === userId)
+    if (userIdx === -1) return Promise.reject('User not found')
+    
+    gUsers.splice(userIdx, 1)
+
+    return _saveUsersToFile()
+}
+
+function _saveUsersToFile() {
+    return new Promise((resolve, reject) => {
+        const usersStr = JSON.stringify(gUsers, null, 4)
+        fs.writeFile(USERS_FILE, usersStr, (err) => {
+            if (err) return reject(err)
+
+            resolve()
+        })
+    })
+}
+
+
+
+
+
+
+
+
